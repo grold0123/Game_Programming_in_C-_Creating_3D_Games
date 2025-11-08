d8bec3df0c2683ba165faf3c606136aedfe0fe54
grold0123 <172453552+grold0123@users.noreply.github.com>
2025-11-08 14:23:57 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,150 +2,178 @@
 #include"SDL3/SDL.h"
 using namespace std;
 
-
-
-int main () {
-    
-    struct color{
+//-------------------------------------------------------------
+//structs
+struct color{
         color():r(255),g(255),b(255),a(255){}
         color(float r,float g,float b,float a):r(r),g(g),b(b),a(a){}
         float r,g,b,a;
-    }
-    bgColor{0,0,255,255};   
-    struct vec2D{
+};
+struct vec2D{
         vec2D():x(0),y(0){}
         vec2D(float a,float b):x(a),y(b){}
         float x,y;
-    }
-    direction,position,velocity,acceleration;
-    const char
-    *title = "initial window";
-    const bool
-    *keystate;
-    int 
-    width = 700 , height = 520, colorIncrement = 150;
-    bool 
-    running = true;
-    float 
-    lastTime = 0,currentTime = 0,deltaTime = 0;
-    SDL_Window
-    *window = nullptr;
-    SDL_Renderer
-    *renderer = nullptr;
-    SDL_Event 
-    event;
-    SDL_FRect 
-    rect{0,0,50,50};
-
-    {//initialize SDL
-        if (!SDL_Init(SDL_INIT_VIDEO)) return -1;
-        if (!SDL_CreateWindowAndRenderer(title,width,height,SDL_WINDOW_RESIZABLE,&window,&renderer)) return -1;        
-    }
-
-    while (running) {//game loop
-        
-        {//poll events
-            while (SDL_PollEvent(&event)) {
-                switch (event.type) {
-                    case SDL_EVENT_QUIT : running = false;
-                    case SDL_EVENT_KEY_UP: switch (event.key.key) {
-                        case SDLK_W : cout << "Pressed W key\n";break;
-                        case SDLK_S : cout << "Pressed S key\n";break;
-                        case SDLK_D : cout << "Pressed D key\n";break;
-                        case SDLK_A : cout << "Pressed A key\n";break;
-                        default: break;
-                    }
-                }
-            }
-        }
-        
-        {//inputs
-            direction = {0,0};
-            keystate = SDL_GetKeyboardState(nullptr);
-            if (keystate[SDL_SCANCODE_S]) direction.y = 1;
-            else if (keystate[SDL_SCANCODE_W]) direction.y = -1;
-
-            if (keystate[SDL_SCANCODE_D]) direction.x = 1;
-            else if (keystate[SDL_SCANCODE_A]) direction.x = -1;
-        }
-        
-        {//update
-
-            currentTime = SDL_GetTicks();
-            deltaTime = currentTime - lastTime;
-            lastTime = currentTime; 
-
-            acceleration.x = direction.x;
-            acceleration.y = direction.y;            
-
-            velocity.x = acceleration.x * deltaTime;
-            velocity.y = acceleration.y * deltaTime;
-
-            vec2D next = {
-                position.x + velocity.x * deltaTime,
-                position.y + velocity.y * deltaTime
+};
+//-------------------------------------------------------------
+//classes
+class Ball{
+public:
+    void move(const float&deltaTime,const int&gameWidth,const int&gameHeight){                
+        {//bounce effect if hitting bounds
+            vec2D nextPosition = {
+                position.x + (direction.x * speed * deltaTime),
+                position.y + (direction.y * speed * deltaTime),
             };
-
-            if  (next.x < 0 || next.x + rect.w > width) {
-                    velocity.x = 0;
+            {//for 0 value x , y  
+                if (nextPosition.x < 0 && direction.x < 0) {
+                    direction.x *= -1;   
+                    position.x = 0;
                 }
-            if  (next.y < 0 || next.y + rect.h > height) {
-                    velocity.y = 0;
+                if (nextPosition.y < 0 && direction.y < 0){
+                    direction.y *= -1;   
+                    position.y = 0;
                 }
-            
-            position.x += velocity.x * deltaTime;
-            position.y += velocity.y * deltaTime;
-
-            rect.x = position.x;
-            rect.y = position.y;
-
-            if (bgColor.b <= 0.0f) {
-                bgColor.b = 0.0f;
-                colorIncrement *= -1;
             }
-
-            if (bgColor.b >= 255.0f){
-                bgColor.b = 255.0f;
-                colorIncrement *= -1;
+            {//for gameWidth and gameHeight 
+                if (nextPosition.x + rect.w > gameWidth  && direction.x > 0){
+                    direction.x *= -1;   
+                    position.x = gameWidth - rect.w;
+                }
+                if (nextPosition.y + rect.h > gameHeight && direction.y > 0){
+                    direction.y *= -1;   
+                    position.y = gameHeight - rect.h;
+                }
             }
-            
-            bgColor.b += colorIncrement * (deltaTime/1000.0f);
-            
         }
-        
-        {//render
-            SDL_SetRenderDrawColor(renderer,static_cast<Uint8>(bgColor.r),static_cast<Uint8>(bgColor.g),static_cast<Uint8>(bgColor.b),static_cast<Uint8>(bgColor.a));
-            cout << bgColor.r << " " << bgColor.g << " " << bgColor.b << endl;
-            SDL_RenderClear(renderer);
-
-            SDL_SetRenderDrawColor(renderer,255,255,255,255);
-            SDL_RenderFillRect(renderer,&rect);
-
-            SDL_RenderPresent(renderer);
-            cout << rect.x << "," << rect.y<< "\n";
+        {//apply delta time to velocity and assign to position
+            position.x += deltaTime * direction.x * speed;
+            position.y += deltaTime * direction.y * speed;
         }
-    
+        {//assign position to rect
+            rect.x = position.x ; 
+            rect.y = position.y ;
+        }                        
+        {//temp check
+            if (direction.x > 0) ballColor.r += 10 * deltaTime;
+            if (direction.x < 0) ballColor.r -= 10 * deltaTime;
+
+            if (direction.y > 0) ballColor.g += 10 * deltaTime;
+            if (direction.y < 0) ballColor.g -= 10 * deltaTime;
+        }
     }
-
-    {//cleanup SDL
+    
+    void draw(SDL_Renderer*renderer)const{
+        SDL_SetRenderDrawColor(
+            renderer,
+            static_cast<Uint8>(ballColor.r),
+            static_cast<Uint8>(ballColor.g),
+            static_cast<Uint8>(ballColor.b),
+            static_cast<Uint8>(ballColor.a)
+        );
+        SDL_RenderFillRect(renderer,&rect);        
+    }
+private:    
+    color ballColor;
+    SDL_FRect rect{0,0,50,50};
+    vec2D direction{1,1},position{rect.x,rect.y},velocity,acceleration;
+    float speed = 1000;
+};
+class Game{
+public:
+    Game(){}
+    bool initialize(){
+        if (!SDL_Init(SDL_INIT_VIDEO)) return false;
+        if (!SDL_CreateWindowAndRenderer(title,gameWidth,gameHeight,SDL_WINDOW_RESIZABLE,&window,&renderer)) return false;        
+        return true;
+    }
+    void run(){        
+        while (running) {            
+            this->pollEvent();
+            this->input();
+            this->update();
+            this->render();
+        }
+    }
+    void shutdown () {
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
     }
+private:
+    const char*title = "Ball class";
+    int gameWidth = 720, gameHeight = 520;
+    color bgColor{255,255,255,255};       
+    const bool*keystate = nullptr;    
+    int  bgColorIncrement = 150;
+    bool running = true;
+    float lastTime = 0,currentTime = 0,deltaTime = 0;
+    SDL_Window*window = nullptr;
+    SDL_Renderer*renderer = nullptr;
+    SDL_Event event;
+    
+    Ball ball;
 
-    return 0;
+    void pollEvent () {        
+        while (SDL_PollEvent(&event)){
+            switch (event.type) {
+                case SDL_EVENT_QUIT : running = false;break;
+                case SDL_EVENT_KEY_UP: switch (event.key.key) {
+                    case SDLK_W : cout << "Pressed W key\n";break;
+                    case SDLK_S : cout << "Pressed S key\n";break;
+                    case SDLK_D : cout << "Pressed D key\n";break;
+                    case SDLK_A : cout << "Pressed A key\n";break;
+                    default: break;
+                }
+            }
+        }
+    }
+    void input () {}
+    void update () {
+        {//get delta time
+            currentTime = SDL_GetTicks();
+            deltaTime = (currentTime - lastTime)/1000.0f ;
+            lastTime = currentTime;
+        }
+        ball.move(deltaTime,gameWidth,gameHeight);
+    }
+    void render () {
+        {//clear renderer with bgColor
+            SDL_SetRenderDrawColor(
+                renderer,
+                static_cast<Uint8>(bgColor.r),
+                static_cast<Uint8>(bgColor.g),
+                static_cast<Uint8>(bgColor.b),
+                static_cast<Uint8>(bgColor.a)
+            );
+            SDL_RenderClear(renderer);
+        }
+        {//renderball
+            this->ball.draw(renderer);
+        }
+        SDL_RenderPresent(renderer);
+    }  
+};
+//-------------------------------------------------------------
+//main function
+int main () {                                                
+    Game game;
+    if (!game.initialize()) return -1;
+    game.run();
+    game.shutdown();
 }
 
 
 
 /*
 --hello world
-g++ src\*.cpp -Llib -Iinclude -lSDL3 -lSDL3_image -o build\hello_world.exe
+g++ src\main.cpp -Llib -Iinclude -lSDL3 -lSDL3_image -o build\hello_world.exe
 
 --initial window
-g++ src\*.cpp -Llib -Iinclude -lSDL3 -lSDL3_image -o build\initial_window.exe
+g++ src\main.cpp -Llib -Iinclude -lSDL3 -lSDL3_image -o build\initial_window.exe
 
 --inputs
-g++ src\*.cpp -Llib -Iinclude -lSDL3 -lSDL3_image -o build\inputs.exe
+g++ src\main.cpp -Llib -Iinclude -lSDL3 -lSDL3_image -o build\inputs.exe
 
+--ball class
+g++ src\main.cpp -Llib -Iinclude -lSDL3 -lSDL3_image -o build\ball_class.exe
 */
